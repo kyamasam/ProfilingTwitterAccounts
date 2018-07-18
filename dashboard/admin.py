6f0451f181faec0292cd91b6702f36317639c6b3
kyamasam <samuelkyama102@gmail.com>
2018-07-18 20:35:15 +0300

    day before presentation

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.site_header = 'Spider Admin'
+
+from .models import Social_Accounts ,Watching_Accounts
+admin.site.register(Watching_Accounts)
+admin.site.register(Social_Accounts)
+
+
+class MyModelAdmin(admin.ModelAdmin):
+    class Media:
+        # js = ('js/admin/my_own_admin.js',)
+        css = {
+             'all': ('css/admin/admin.css',)
+        }
